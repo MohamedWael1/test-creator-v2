d8d345370f4446c0c932f66cf9dbddebf0442cf5
MohamedWael1 <mohwael00@gmail.com>
2023-07-16 16:42:15 +0200

    redirect to dashboard

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  redirects: async () => {
+    return [
+      {
+        source: '/',
+        destination: '/dashboard/tests',
+        permanent: true,
+      },
+    ]
+  }
 }
 
 module.exports = nextConfig
--- pages/index.tsx
+++ pages/index.tsx
@@ -30,3 +30,5 @@ export default function Home() {
         </WithPrivate>
     );
 }
+
+
